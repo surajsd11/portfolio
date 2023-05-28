603746d951dfb5932ab62830e3b9004b88164392
suraj <surajsinghdev2003@gamil.com>
2023-05-28 13:52:54 +0530

    first commit

---
+++ src/components/Sidebar/index.js
@@ -0,0 +1,77 @@
+import './index.scss'
+import { Link, NavLink } from 'react-router-dom'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import LogoS from '../../assets/images/logo-s.png'
+import {
+  faLinkedin,
+  faGithub,
+  faYoutube,
+  faSkype,
+} from '@fortawesome/free-brands-svg-icons'
+import {
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from '@fortawesome/free-solid-svg-icons'
+
+const Sidebar = () => (
+  <div className="nav-bar">
+    <Link className="logo" to="/">
+      <img src={LogoS} alt="logo" />
+    </Link>
+    <nav>
+      <NavLink exact="true" activeclassname="active" to="/">
+        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+      </NavLink>
+      <NavLink activeclassname="active" className="about-link" to="/about">
+        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+      </NavLink>
+      <NavLink
+        activeclassname="active"
+        className="contact-link"
+        to="/contact"
+      >
+        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+      </NavLink>
+    </nav>
+    <ul>
+      <li>
+        <a
+          href="https://www.linkedin.com/in/suraj-singh-09986921b"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <FontAwesomeIcon icon={faLinkedin} color="#4d4d4e" />
+        </a>
+      </li>
+      <li>
+          <a
+            href="https://github.com/surajsd11"
+            target="_blank"
+            rel="noreferrer"
+          >
+            <FontAwesomeIcon
+              icon={faGithub}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+      
+        <li>
+          <a href="skype:live:bobangajicsm" rel="noreferrer" target="_blank">
+            <FontAwesomeIcon
+              icon={faSkype}
+              color="#4d4d4e"
+              className="anchor-icon"
+            />
+          </a>
+        </li>
+    </ul>
+  </div>
+)
+
+export default Sidebar
